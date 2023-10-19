2166f5140c8d64fb45a961829585377826669b87
PacKaddin <papay@stud.uniza.sk>
2023-10-19 12:31:32 +0200

    ggggg

--- server.py
+++ server.py
@@ -4,4 +4,11 @@ import json
 from enum import IntEnum
 
 SERVER = "0.0.0.0"
-PORT = 80
+PORT = 8080
+MLEN = 1000
+QUEUE_LENGTH = 10
+
+class Operacia(intEnum):
+    LOGIN = 1
+    EXIT = 2
+    USERS = 3

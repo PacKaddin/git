22a39b2d63394a4271cbd5103aeb8e095af782dc
PacKaddin <papay@stud.uniza.sk>
2023-10-19 13:17:59 +0200

    Zaloha 5 electric boogaloo

--- server.py
+++ server.py
@@ -33,17 +33,31 @@ def VybavKlienta(paClientSocket, paClientAddr, paPouzivatelia):
 
         if message.operacia == Operacia.LOGIN:
             paPouzivatelia.append(message.od)
-            print("Prihlasil sa {} z IP {}, port {}".format{message.od, paClientAddr[0], paClientAddr[1]})
+            print("Prihlasil sa {} z IP {}, port {}".format(message.od, paClientAddr[0], paClientAddr[1]))
             continue
 
         if message.operacia == Operacia.EXIT:
             paPouzivatelia.remove(message.od)
-            print("Odhlasil sa {} z IP {}, port {}".format{message.od, paClientAddr[0], paClientAddr[1]})
-            continue
+            print("Odhlasil sa {} z IP {}, port {}".format(message.od, paClientAddr[0], paClientAddr[1]))
+            return
 
         if message.operacia == Operacia.USERS:
             odpoved = Sprava("SERVER", message.od, Operacia.USERS.value, paPouzivatelia)
             jsonStr = json.dumps(odpoved.__dict__)
             paClientSocket.send(jsonStr.encode())
+            continue
 
 
+if __name__ == "__main__":
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((SERVER, PORT))
+    sock.listen(QUEUE_LENGTH)
+
+    pouzivatelia = list()
+
+    print("Spustil sa server")
+
+    while True:
+        (clientSock, clientAddr) = sock.accept()
+        t = Thread(target=VybavKlienta, args=(clientSock, clientAddr, pouzivatelia))
+        t.start()

bb4d41a690f68ad9e532dbdc7daf72c469c9de02
PacKaddin <papay@stud.uniza.sk>
2023-10-19 12:47:07 +0200

    zaloha 3 ps: pomoc

--- server.py
+++ server.py
@@ -19,3 +19,15 @@ class Sprava:
         self.komu = paKomu
         self.operacia = paOperacia
         self.text = paText
+
+    @staticmethod
+    def json_decoder(paObj):
+        return Sprava(paObj['od'], paObj['komu'], paObj['operacia'], paObj['text'])
+
+def VybavKlienta(paClientSocket, paClientAddr, paPouzivatelia):
+
+    while(True):
+        sprava = paClientSocket.recv(MLEN)
+        jsonStr = sprava.decode()
+        message = json.loads(jsonStr, object_hook=Sprava.json_decoder)
+
